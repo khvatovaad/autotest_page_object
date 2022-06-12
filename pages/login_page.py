a7fc1c189e091fbcaaf999dcfe9decfd9680906c
Anastasiya Khvatova <khvatova.ad@gmail.com>
2022-06-12 23:22:53 +1000

    page object: login page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from selenium.common.exceptions import NoSuchElementException
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        try:
+            self.browser.current_url = LoginPageLocators.LOGIN_URL
+        except NoSuchElementException:
+            return False
+        assert True
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
