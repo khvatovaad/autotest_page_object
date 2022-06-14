23f506f68cc3cc6b5983eb756cbd9ae54a3a3850
anastasiya <khvatova.ad@dns-shop.ru>
2022-06-14 11:56:17 +1000

    login from main to base

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,20 @@ from .pages.main_page import MainPage
 link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    linkProductPage = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, linkProductPage)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    linkProductPage = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, linkProductPage)
+    page.open()
+    page.go_to_login_page()
+
+
 def test_guest_can_add_product_to_basket(browser):
     page = MainPage(browser, link)
     page.open()
@@ -16,6 +30,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     product_page.should_be_price_in_basket()
     product_page.should_be_name_book_in_basket()
+
 
 @pytest.mark.skip()
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
