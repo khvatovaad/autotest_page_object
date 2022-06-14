acb2db49141ae601f3f180cc374b1a97c2a38573
anastasiya <khvatova.ad@dns-shop.ru>
2022-06-14 11:34:56 +1000

    negative cases

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,3 +13,11 @@ class ProductPage(BasePage):
     def should_be_name_book_in_basket(self):
         assert self.is_element_present(*ProductPageLocators.BOOK_NAME), "Book name is not presented"
         assert self.browser.find_element(*ProductPageLocators.BOOK_NAME).text == self.browser.find_element(*ProductPageLocators.BOOK_NAME_MASSAGE).text,  "Book name not in basket, error"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.BOOK_NAME_MASSAGE), \
+        "Success message is presented, but should not be"
+
+    def should_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.BOOK_NAME_MASSAGE), \
+        "Success message is not disappeared, but should be"
--- test_product_page.py
+++ test_product_page.py
@@ -5,17 +5,10 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.main_page import MainPage
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+
+def test_guest_can_add_product_to_basket(browser):
     page = MainPage(browser, link)
     page.open()
     product_page = ProductPage(browser, browser.current_url)
@@ -23,3 +16,32 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     product_page.should_be_price_in_basket()
     product_page.should_be_name_book_in_basket()
+
+@pytest.mark.skip()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = MainPage(browser, link)
+    product_page = ProductPage(browser, browser.current_url)
+
+    page.open()
+    product_page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = MainPage(browser, link)
+    product_page = ProductPage(browser, browser.current_url)
+
+    page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.skip()
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = MainPage(browser, link)
+    product_page = ProductPage(browser, browser.current_url)
+
+    page.open()
+    product_page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    product_page.should_disappeared_success_message()
